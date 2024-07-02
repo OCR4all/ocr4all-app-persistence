ce52942e64ec7ad4b7f3367ecb2547c9e1dd886b
Herbert Baier <baier@itbaier.de>
2024-07-02 14:19:13 +0200

    model/recognition model

--- src/main/java/de/uniwuerzburg/zpd/ocr4all/application/persistence/spi/RecognitionModelArgument.java
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/persistence/spi/RecognitionModelArgument.java
@@ -7,6 +7,8 @@
  */
 package de.uniwuerzburg.zpd.ocr4all.application.persistence.spi;
 
+import java.util.List;
+
 /**
  * Defines recognition model arguments.
  *
@@ -14,10 +16,112 @@ package de.uniwuerzburg.zpd.ocr4all.application.persistence.spi;
  * @version 1.0
  * @since 1.8
  */
-public class RecognitionModelArgument extends MultipleValueArgument<String> {
+public class RecognitionModelArgument extends Argument {
 	/**
 	 * The serial version UID.
 	 */
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The assembles.
+	 */
+	private List<Assemble> assembles;
+
+	/**
+	 * Returns the assembles.
+	 *
+	 * @return The assembles.
+	 * @since 17
+	 */
+	public List<Assemble> getAssembles() {
+		return assembles;
+	}
+
+	/**
+	 * Set the assembles.
+	 *
+	 * @param assembles The assembles to set.
+	 * @since 17
+	 */
+	public void setAssembles(List<Assemble> assembles) {
+		this.assembles = assembles;
+	}
+
+	/**
+	 * Model is an immutable class that defines assembles.
+	 *
+	 * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+	 * @version 1.0
+	 * @since 17
+	 */
+	public static class Assemble {
+		/**
+		 * The id.
+		 */
+		private String id;
+
+		/**
+		 * The models.
+		 */
+		private List<String> models;
+
+		/**
+		 * Creates an assemble.
+		 * 
+		 * @param id     The id.
+		 * @param models The models.
+		 * @since 17
+		 */
+		public Assemble(String id, List<String> models) throws IllegalArgumentException {
+			super();
+
+			if (id == null || id.isBlank())
+				throw new IllegalArgumentException("the assemble argument id can not be empty.");
+			this.id = id;
+
+			this.models = models;
+		}
+
+		/**
+		 * Returns the id.
+		 *
+		 * @return The id.
+		 * @since 17
+		 */
+		public String getId() {
+			return id;
+		}
+
+		/**
+		 * Set the id.
+		 *
+		 * @param id The id to set.
+		 * @since 17
+		 */
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		/**
+		 * Returns the models.
+		 *
+		 * @return The models.
+		 * @since 17
+		 */
+		public List<String> getModels() {
+			return models;
+		}
+
+		/**
+		 * Set the models.
+		 *
+		 * @param models The models to set.
+		 * @since 17
+		 */
+		public void setModels(List<String> models) {
+			this.models = models;
+		}
+
+	}
 
 }
